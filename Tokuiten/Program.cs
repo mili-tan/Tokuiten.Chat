703411a00ec37e1f6ca8fd18022e1e8f00acdb0b
Milkey Tan <819774989@qq.com>
2019-04-24 16:13:07 +0800

    join+leave

--- Tokuiten/Program.cs
+++ Tokuiten/Program.cs
@@ -1,37 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using Fleck;
+using MojoUnity;
 
 namespace Tokuiten
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             var allConnection = new List<IWebSocketConnection>();
+            var allChat = new Dictionary<IWebSocketConnection, UserEntity>();
             var wsServer = new WebSocketServer("ws://0.0.0.0:23333");
             wsServer.Start(socket =>
             {
                 socket.OnOpen = () =>
                 {
                     Console.WriteLine(socket.ConnectionInfo.Id + ": " + "Open!");
-                    socket.Send("");
                     allConnection.Add(socket);
                 };
                 socket.OnClose = () =>
                 {
                     Console.WriteLine(socket.ConnectionInfo.Id + ": " + "Close!");
-                    socket.Send("");
+
+                    if (allChat.ContainsKey(socket))
+                    {
+                        foreach (var item in allChat)
+                            if (item.Value.Channel == allChat[socket].Channel)
+                                item.Key.Send($"{allChat[socket].Nick}:leave");
+
+                        allChat.Remove(socket);
+                    }
+
                     allConnection.Remove(socket);
                 };
                 socket.OnMessage = msg =>
                 {
-
+                    if (msg.Contains("cmd"))
+                    {
+                        JsonValue jMsg = Json.Parse(msg);
+                        var cmd = jMsg.AsObjectGetString("cmd");
+                        if (cmd == "join")
+                        {
+                            var channel = jMsg.AsObjectGetString("channel");
+                            var nick = jMsg.AsObjectGetString("nick");
+                            if (allChat.TryAdd(socket, new UserEntity() {Channel = channel, Nick = nick}))
+                                foreach (var item in allChat)
+                                    if (item.Value.Channel == channel)
+                                        item.Key.Send($"{allChat[socket].Nick}:join");
+                        }
+                    }
+                    
                 };
 
             });
             Console.ReadKey();
+        }
+        public class UserEntity
+        {
+            public string Nick;
+            public string Channel;
         }
     }
 }

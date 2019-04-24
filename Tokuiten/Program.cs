09fcbdfd77c0f5ab0c08fedd89c465f1c8c515db
Milkey Tan <819774989@qq.com>
2019-04-24 10:18:26 +0800

    添加项目文件。

---
+++ Tokuiten/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Fleck;
+
+namespace Tokuiten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var allConnection = new List<IWebSocketConnection>();
+            var wsServer = new WebSocketServer("ws://0.0.0.0:23333");
+            wsServer.Start(socket =>
+            {
+                socket.OnOpen = () =>
+                {
+                    Console.WriteLine(socket.ConnectionInfo.Id + ": " + "Open!");
+                    socket.Send("");
+                    allConnection.Add(socket);
+                };
+                socket.OnClose = () =>
+                {
+                    Console.WriteLine(socket.ConnectionInfo.Id + ": " + "Close!");
+                    socket.Send("");
+                    allConnection.Remove(socket);
+                };
+                socket.OnMessage = msg =>
+                {
+
+                };
+
+            });
+            Console.ReadKey();
+        }
+    }
+}

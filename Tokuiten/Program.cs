03b6742a12ef9af6e8b6417f7e55b2af8805b0b9
Milkey Tan <819774989@qq.com>
2019-04-25 09:50:07 +0800

    update

--- Tokuiten/Program.cs
+++ Tokuiten/Program.cs
@@ -9,7 +9,6 @@ namespace Tokuiten
     {
         static void Main()
         {
-            var allConnection = new List<IWebSocketConnection>();
             var allChat = new Dictionary<IWebSocketConnection, UserEntity>();
             var wsServer = new WebSocketServer("ws://0.0.0.0:23333");
             wsServer.Start(socket =>
@@ -17,7 +16,7 @@ namespace Tokuiten
                 socket.OnOpen = () =>
                 {
                     Console.WriteLine(socket.ConnectionInfo.Id + ": " + "Open!");
-                    allConnection.Add(socket);
+                    Console.WriteLine(socket.ConnectionInfo.Path);
                 };
                 socket.OnClose = () =>
                 {
@@ -27,12 +26,10 @@ namespace Tokuiten
                     {
                         foreach (var item in allChat)
                             if (item.Value.Channel == allChat[socket].Channel)
-                                item.Key.Send($"{allChat[socket].Nick}:leave");
+                                item.Key.Send($"{allChat[socket].Nick}:leave:{Guid.NewGuid()}");
 
                         allChat.Remove(socket);
                     }
-
-                    allConnection.Remove(socket);
                 };
                 socket.OnMessage = msg =>
                 {
@@ -40,21 +37,52 @@ namespace Tokuiten
                     {
                         JsonValue jMsg = Json.Parse(msg);
                         var cmd = jMsg.AsObjectGetString("cmd");
-                        if (cmd == "join")
+                        switch (cmd)
                         {
-                            var channel = jMsg.AsObjectGetString("channel");
-                            var nick = jMsg.AsObjectGetString("nick");
-                            if (allChat.TryAdd(socket, new UserEntity {Channel = channel, Nick = nick}))
-                                foreach (var item in allChat)
-                                    if (item.Value.Channel == channel)
-                                        item.Key.Send($"{allChat[socket].Nick}:join");
-                        }
-                        if (cmd == "chat")
-                        {
-                            var text = jMsg.AsObjectGetString("text");
+                            case "join":
+                            {
+                                var channel = jMsg.AsObjectGetString("channel");
+                                var nick = jMsg.AsObjectGetString("nick");
+                                if (allChat.TryAdd(socket, new UserEntity {Channel = channel, Nick = nick}))
+                                    foreach (var item in allChat)
+                                        if (item.Value.Channel == channel)
+                                            item.Key.Send($"{allChat[socket].Nick}:join:{Guid.NewGuid()}");
+                                break;
+                            }
+                            case "chat":
+                            {
+                                var text = jMsg.AsObjectGetString("text");
                                 foreach (var item in allChat)
                                     if (item.Value.Channel == allChat[socket].Channel)
-                                        item.Key.Send($"{allChat[socket].Nick}:{text}");
+                                        item.Key.Send($"{allChat[socket].Nick}:{text}:{Guid.NewGuid()}");
+                                break;
+                            }
+                            case "whisper":
+                            {
+                                var toWho = jMsg.AsObjectGetString("to");
+                                var text = jMsg.AsObjectGetString("text");
+                                foreach (var item in allChat)
+                                    if (item.Value.Channel == allChat[socket].Channel && item.Value.Nick == toWho)
+                                        item.Key.Send($"{allChat[socket].Nick}:{text}:{Guid.NewGuid()}");
+                                break;
+                            }
+                            case "delete":
+                            {
+                                var cid = jMsg.AsObjectGetString("cid");
+                                foreach (var item in allChat)
+                                    if (item.Value.Channel == allChat[socket].Channel)
+                                        item.Key.Send($"{allChat[socket].Nick}:delete:{cid}");
+                                break;
+                            }
+                            case "edit":
+                            {
+                                var cid = jMsg.AsObjectGetString("cid");
+                                var text = jMsg.AsObjectGetString("text");
+                                foreach (var item in allChat)
+                                    if (item.Value.Channel == allChat[socket].Channel)
+                                        item.Key.Send($"{allChat[socket].Nick}:edit:{cid}:{text}");
+                                break;
+                            }
                         }
                     }
                     
